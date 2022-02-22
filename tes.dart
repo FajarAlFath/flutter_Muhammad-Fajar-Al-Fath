fce38c1b6e70ed5c91469d3409b425a256cb9749
FajarAlFath <99872320+FajarAlFath@users.noreply.github.com>
2022-02-22 17:27:17 +0700

    Add files via upload

---
+++ tes.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  print('halo wordl');
+  print('ini percobaan push ke git');
+  print('ini ditamhabahkan untuk melihat staging di git');
+  print('merupakan perubahan awal di stage');
+  int number = 12;
+}
