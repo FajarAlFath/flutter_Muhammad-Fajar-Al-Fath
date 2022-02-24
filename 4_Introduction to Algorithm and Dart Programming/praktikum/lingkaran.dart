3ebd104c6df18399dc332d2f781ad46f72370667
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-02-24 19:08:47 +0700

    penambahan komentar

--- 4_Introduction to Algorithm and Dart Programming/praktikum/lingkaran.dart
+++ 4_Introduction to Algorithm and Dart Programming/praktikum/lingkaran.dart
@@ -1,13 +1,18 @@
+// libary for input output
 import 'dart:io';
+
+// class main
 void main() {
   print('===== Program mencari luas lingkaran =====');
   stdout.write('Tentukan nilai r = ');
   double s = double.parse(stdin.readLineSync()!);
 
+  // pemanggilan fungsi rumus
   double hasil = rumus(s);
   print('Luas lingkaran tersebut adalah $hasil');
 } 
 
+// inisialisasi fungsi rumus
 double rumus(double r){
   double phi = 3.14;
   return phi*r*r;
