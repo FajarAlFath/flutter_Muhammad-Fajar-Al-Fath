6c7cdb4af3e4e9d41c6ec7a75239284a84820804
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-02-24 18:19:17 +0700

    penggabungan berbagai string

---
+++ 4_Introduction to Algorithm and Dart Programming/praktikum/String.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi';
+void main() {
+  print('===== Menampilkan berbagai string =====');
+
+  //inisialisasi variable
+  var a = 'kami senang';
+  var b = 'belajar bahasa';
+  var c = 'pemograman dart';
+
+  //menampilkan output
+  print('$a $b $c.');
+}
