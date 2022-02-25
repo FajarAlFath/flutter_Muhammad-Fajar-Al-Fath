539b5c622d3d1a252ef943fdaf1767d855b22309
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-02-25 17:33:00 +0700

    update file section 5

---
+++ 5_Basic Dart Programming/ praktikum/string.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('===== Menampilkan berbagai string =====');
+
+  //inisialisasi variable
+  var a = 'kami senang';
+  var b = 'belajar bahasa';
+  var c = 'pemograman dart';
+
+  //menampilkan output
+  print('$a $b $c.');
+}
