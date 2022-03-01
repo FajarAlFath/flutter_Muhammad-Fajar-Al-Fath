3a6cf8093ba7333843860ade1082ab11ec28a7b3
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-01 15:46:50 +0700

    soal 2 praktikum

--- 6_Branching - Looping - Function/praktikum/2.dart
+++ 6_Branching - Looping - Function/praktikum/2.dart
@@ -1,18 +1,18 @@
-//impoort library
+//import library
 import 'dart:io';
 
 //fungsi faktorial
 faktorial (int f){
   int a = 1;
-  for (var i = 0; i <= f; i++) {
+  for (var i = 1; i <= f; i +=1) {
     a *= i;
   } 
   return a;
 }
 void main() {
-  print('===> Program pemfaktoran <===');
+  print('===> Program pemfaktoran berbasis input output <===');
   print('input nilai: ');
-
   int? f = int.parse(stdin.readLineSync()!);
   int hasil = faktorial(f); 
+  print('hasil faktorial adalah $hasil');
 }
