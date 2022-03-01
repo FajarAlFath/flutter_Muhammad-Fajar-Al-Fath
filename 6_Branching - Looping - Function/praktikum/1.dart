37c0d3a5e0ff769d450582187c76aef91bed525f
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-01 14:50:08 +0700

    praktikum soal no 1

---
+++ 6_Branching - Looping - Function/praktikum/1.dart
@@ -0,0 +1,43 @@
+//import library
+import 'dart:io';
+
+//function nilai
+nilai(int n){
+  String value;
+  if(n > 70){
+    return value = 'A';
+  }
+  else if (n > 40){
+   return value = 'B';
+  }
+  else if (n > 0){
+    return value = 'C';
+  }
+  else{
+    return value = 'kosong';
+  }
+}
+
+//function lulus
+lulus(String hasil){
+  String pesan;
+  if(hasil == 'A'){
+    return pesan = 'Selamat anda Cumlaude';
+  }
+  else if(hasil == 'B'){
+    return pesan = 'Selamat anda lulus';
+  }
+  else if(hasil == 'C'){
+    return pesan = 'Mohon maaf anda gagal, mohon coba lagi';
+  }
+  else{
+    return pesan = 'Mohon maaf anda tidak lulus';
+  }
+}
+
+void main(){
+  print ('masukkan nilai : ');
+  int? n = int.parse(stdin.readLineSync()!);
+  var hasil = nilai (n);
+  print ('nilai anda $hasil');
+  }
