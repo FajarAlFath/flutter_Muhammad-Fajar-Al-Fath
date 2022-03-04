60f5f991ceacd786c972abd81f5cad7d3e8dc42f
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-04 16:55:54 +0700

    up screenshots of task

--- 7_Advance Function - Async - Await/praktikum/1.dart
+++ 7_Advance Function - Async - Await/praktikum/1.dart
@@ -7,7 +7,7 @@ Future f1(var a, var kali)async{
       var e = c*kali;
       d.add(e);
     }
-    print(d);
+    print('list setelah dikali $d');
   });
 }
 
@@ -27,6 +27,7 @@ Future f1(var a, var kali)async{
 void main()async{
   print('=============== Soal no 1 ===============');
   var a = [1,2,3,4,5,6,7,8,9,10];
+  print('List awal $a');
   await f1(a, 10);
   
 }
