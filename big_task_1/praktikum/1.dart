b54c98c9ac7fab74edd3b73b478fc6b9fc459314
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-06 10:55:16 +0700

    revisi soal no 1 bagian return value

--- big_task_1/praktikum/1.dart
+++ big_task_1/praktikum/1.dart
@@ -1,15 +1,15 @@
-String cek(int n){
+bool cek(int n){
   if (n % 2 == 0) {
-    return ('bilangan genap');
+    return true;
   }
   else{
-    return ('bilangan ganjil');
+    return false;
   }
 }
 
 void main(){
   var hasil = cek(8);
-  print('angka 8 merupakan $hasil');
+  print('angka 8 merupakan bilangan genap? $hasil');
   hasil = cek(9);
-  print('angka 9 merupakan $hasil');
+  print('angka 9 merupakan bilangan genap? $hasil');
 }
