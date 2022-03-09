e2b021aa7f544840f5b9c49d836e34700b7a9467
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-09 15:38:09 +0700

    revisi code soal no 1 dan 2 section 9

--- 9_Dart Object Oriented Programming 1/praktikum/1.dart
+++ 9_Dart Object Oriented Programming 1/praktikum/1.dart
@@ -1,4 +1,4 @@
-//soal no 1
+//soal no 1 dan no 2 digabung
 import 'dart:io';
 
 class Berat_hewan{
@@ -6,35 +6,48 @@ class Berat_hewan{
   var h0;
   var h1;
   var h2;
-  var h3;
 }
 
 class Mobil{
-  //berat dalam kg
+  // property berat max dalam kg
   int maxkg = 100;
+  int temp = 0;
+
+  //property list nama;
   var list_hewan = [];
 
   //method tambah muatan
-  tambah(String hewan){
+  tambah(String hewan, int berat){
     if (maxkg != 0){
       list_hewan.add(hewan);
+      maxkg -= berat;
     }
+    temp += berat; 
   }
 
   //method total muatan
   total(){
-
+    print(temp);
   }
 }
 
 void main(List<String> args) {
+  //inisialisasi object 
   var berat_hewan = Berat_hewan();
   var mobil = Mobil();
+
+  //inisialisasi property melalui object
   berat_hewan.h0 = 10;
-  print(berat_hewan.h0);
-  mobil.list_hewan.add('kucing');
-  print(mobil.list_hewan[0]);
+  berat_hewan.h1 = 20;
+  berat_hewan.h2 = 30;
 
+  //using method tambah to add
+  mobil.tambah('anjing', berat_hewan.h0);
+  mobil.tambah('kambing', berat_hewan.h1);
+  mobil.tambah('rusa', berat_hewan.h2);
+  print('daftar hewan yang diangkut = ${mobil.list_hewan}');
 
-
+  //using method total to sum
+  stdout.write('total kg hewan yang diangkut mobil = ');
+  mobil.total();
 }
