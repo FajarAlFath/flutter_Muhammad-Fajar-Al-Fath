0841ca966f8f27b66fea90cb0d47e75c83a44c30
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-11 19:45:20 +0700

    revisi balok

--- 10_Dart Object Oriented Programming 2/praktikum/balok.dart
+++ 10_Dart Object Oriented Programming 2/praktikum/balok.dart
@@ -2,10 +2,7 @@ import 'bangunruang.dart';
 
 class Balok extends Bangunruang{
   //setiap property dalam cm
-  var panjang;
-  var tinggi;
-  var lebar;
-
+ 
   @override
   volume() {
     var hasil = panjang*tinggi*lebar;
