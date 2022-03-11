804fe634a10d57f74f772fb0b24c20d14678d8f0
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-11 15:17:06 +0700

    using stdin to io

--- 10_Dart Object Oriented Programming 2/praktikum/bangunruang.dart
+++ 10_Dart Object Oriented Programming 2/praktikum/bangunruang.dart
@@ -1,5 +1,6 @@
 import 'balok.dart';
 import 'kubus.dart';
+import 'dart:io';
 
 class Bangunruang{
   volume(){
@@ -10,4 +11,7 @@ class Bangunruang{
 void main(List<String> args) {
   var balok = Balok();
   var kubus = Kubus();
+
+  stdout.write('input sisi');
+  kubus.sisi = int.parse(stdin.readLineSync()!);
 }
