4b5d354c5233136903db8fb38b39b93ba8d9aea6
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-11 15:04:35 +0700

    progress task 1

---
+++ 10_Dart Object Oriented Programming 2/praktikum/bangunruang.dart
@@ -0,0 +1,13 @@
+import 'balok.dart';
+import 'kubus.dart';
+
+class Bangunruang{
+  volume(){
+    print ('Menghitung Volume');
+  }  
+}
+
+void main(List<String> args) {
+  var balok = Balok();
+  var kubus = Kubus();
+}
