c8e64c620c4dab5e2140c791cbbbe347b75343eb
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-17 18:51:45 +0700

    revisi code watch

--- 11_Introduction flutter Widget/praktikum/flutter1/lib/main.dart
+++ 11_Introduction flutter Widget/praktikum/flutter1/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:html';
 import 'package:flutter/material.dart';
 import 'dart:core';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(new MyApp());
@@ -13,38 +14,37 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyAppState extends State{
-  var jam = 'Jam';
-
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Simple Watch for Alterra'),
-        ),
-        body: Center(
-          child: Text(
-            jam,
-            style: TextStyle(fontSize: 50),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-void Mulai() {
-  Timer.periodic(new Duration(seconds: 1),(_){
-    var date = new DateTime.now();
-    var formatedjam = new DateFormat.Hms().format(date);
-    setState((){
-      jam = formatedjam;
+  var jam = 'JAM';
+  void Mulai() {
+    Timer.periodic(new Duration(seconds: 1),(_){
+      var date = new DateTime.now();
+      var formatedjam = new DateFormat.Hms().format(date);
+      setState((){
+        jam = formatedjam;
+      });
     });
-  });
-}
+  }
 
  @override 
- void iniState(){
+ void initState(){
    Mulai();
-   super.iniState();
+   super.initState();
  }
+
+@override
+Widget build(BuildContext context) {
+  return new MaterialApp(
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Simple Watch for Alterra'),
+       ),
+      body: Center(
+        child: Text(
+          jam,
+          style: TextStyle(fontSize: 50),
+         ),
+       ),
+     ),
+   );
+  }
+}
