0bce13332e1b99c26a7ba898cec3db2de152edc0
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-20 16:21:39 +0700

    cupertino revisi

--- 12_Platform Widget/praktikum/flutter_cupertino/lib/chats.dart
+++ 12_Platform Widget/praktikum/flutter_cupertino/lib/chats.dart
@@ -1,13 +1,31 @@
+import 'package:cupertino_list_tile/cupertino_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'model.dart';
 
 class Chats extends StatelessWidget{
   const Chats({Key? key}) : super(key: key);
   
   @override 
   Widget build(BuildContext context){
-    return Container(
-      child: Center(
-        child: Text('chats'),
-    ),);
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        return CupertinoListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundImage: NetworkImage(items[index].url),
+          ),
+          title: Text(
+            items[index].nama,
+            style: const TextStyle(fontWeight: FontWeight.w600),
+          ),
+          subtitle: Text(items[index].pesan),
+          trailing: Text(items[index].waktu),
+        );
+      },
+      separatorBuilder: (context, index){
+        return const Divider();
+      },
+      itemCount: items.length,
+    );
   }
 }
