74dd4b50f8cc3046b3a4f2da0ee4b398f4f06af6
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-21 15:42:35 +0700

    push task1

--- 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task1/lib/main.dart
+++ 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task1/lib/main.dart
@@ -1,39 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart';
 
-void main() =>runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return MaterialApp(
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Homepage(),
     );
   }
 }
 
-class Homepage extends StatelessWidget{
-  
-  @override 
-  Widget build(BuildContext context){
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Chip(
-              padding: EdgeInsets.all(0),
-              backgroundColor: Colors.green,
-              label: Text('Badge Chip',style: TextStyle(color: Colors.white),),
+      body: Column(
+        children: <Widget>[
+          const Chip(
+            padding: EdgeInsets.all(0),
+            backgroundColor: Colors.green,
+            label: Text(
+              'Badge Chip',
+              style: TextStyle(color: Colors.white),
             ),
-            Badge(
-              toAnimate: false,
-              shape: BadgeShape.square,
-              borderRadius: BorderRadius.circular(8),
-              badgeContent: Text('Badge', style: TextStyle(color: Colors.white),),
-              badgeColor: Color.fromARGB(255, 107, 3, 126),
-            )
-          ],
-        ),
+          ),
+          Badge(
+            toAnimate: false,
+            shape: BadgeShape.square,
+            borderRadius: BorderRadius.circular(8),
+            badgeContent: const Text(
+              'Badge',
+              style: TextStyle(color: Colors.white),
+            ),
+            badgeColor: const Color.fromARGB(255, 107, 3, 126),
+          )
+        ],
       ),
     );
   }
