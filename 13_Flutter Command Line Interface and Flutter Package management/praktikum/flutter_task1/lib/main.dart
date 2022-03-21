ce329acb92d36f7222b3c45f83f3837469cdc98a
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-21 19:47:58 +0700

    revisi code

--- 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task1/lib/main.dart
+++ 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task1/lib/main.dart
@@ -26,32 +26,31 @@ class Homepage extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
+        
         children: <Widget>[
-          const Chip(
-            padding: EdgeInsets.all(0),
-            backgroundColor: Colors.green,
-            label: Text(
-              'Badge Chip',
-              style: TextStyle(color: Colors.white),
+          const Center(
+            child: Chip(
+              padding: EdgeInsets.all(10),
+              backgroundColor: Colors.green,
+              label: Text(
+                'Badge Chip',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Badge(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                toAnimate: false,
-                shape: BadgeShape.square,
-                borderRadius: BorderRadius.circular(12),
-                badgeContent: const Text(
-                  'Badge',
-                  style: TextStyle(color: Colors.white),
-                ),
-                badgeColor: const Color.fromARGB(255, 107, 3, 126),
+          Center(
+            child: Badge(
+              padding: const EdgeInsets.all(8),
+              toAnimate: false,
+              shape: BadgeShape.square,
+              borderRadius: BorderRadius.circular(12),
+              badgeContent: const Text(
+                'Badge',
+                style: TextStyle(color: Colors.white),
               ),
-            ],
+              badgeColor: const Color.fromARGB(255, 107, 3, 126),
+            ),
           ),
         ],
       ),
