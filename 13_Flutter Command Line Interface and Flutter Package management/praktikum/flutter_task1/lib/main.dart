47cf78f5bb00ee9a0d73ee51b9bf1b05354a095a
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-21 15:38:37 +0700

    push task1

--- 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task1/lib/main.dart
+++ 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task1/lib/main.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 
 void main() =>runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
-    return MaterialApp();
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget{
+  
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Chip(
+              padding: EdgeInsets.all(0),
+              backgroundColor: Colors.green,
+              label: Text('Badge Chip',style: TextStyle(color: Colors.white),),
+            ),
+            Badge(
+              toAnimate: false,
+              shape: BadgeShape.square,
+              borderRadius: BorderRadius.circular(8),
+              badgeContent: Text('Badge', style: TextStyle(color: Colors.white),),
+              badgeColor: Color.fromARGB(255, 107, 3, 126),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
