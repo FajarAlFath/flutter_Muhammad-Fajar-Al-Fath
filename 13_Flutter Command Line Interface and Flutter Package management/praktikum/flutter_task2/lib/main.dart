b3690d5610910cd6dc8793710fc099c1b7dfa438
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-21 17:07:05 +0700

    push flutter task2

--- 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task2/lib/main.dart
+++ 13_Flutter Command Line Interface and Flutter Package management/praktikum/flutter_task2/lib/main.dart
@@ -1,26 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_widget/barcode_widget.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override 
-  Widget build(BuildContext context){
-    return MaterialApp(
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Homepage(),
       debugShowCheckedModeBanner: false,
     );
   }
-  
 }
 
-class Homepage extends StatelessWidget{
+class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
-  @override 
-  Widget build(BuildContext context){
-    return Scaffold(
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Barcode demo simple'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: ListView(
+        children: [
+         Container( 
+           padding: const EdgeInsets.all(100),
+           child: BarcodeWidget(
+            height: 100,
+            width: 100,
+            data: 'Alterra Academy',
+            barcode: Barcode.code128(escapes: true),
+            errorBuilder: (context, error) => Center(child: Text(error)),
+          ),),
+          Container( 
+           padding: const EdgeInsets.all(100),
+           child: BarcodeWidget(
+            height: 100,
+            width: 100,
+            data: 'Flutter Asyik',
+            barcode: Barcode.code128(escapes: true),
+            errorBuilder: (context, error) => Center(child: Text(error)),
+          ),),
+          Container( 
+           padding: const EdgeInsets.all(100),
+           child: BarcodeWidget(
+            height: 100,
+            width: 100,
+            data: 'Muhammad Fajar Al Fath',
+            barcode: Barcode.code128(escapes: true),
+            errorBuilder: (context, error) => Center(child: Text(error)),
+          ),),
+        ],
+      ),
     );
   }
 }
