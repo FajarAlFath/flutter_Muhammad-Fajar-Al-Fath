943dd5d5ae0208a35f87470ae23d25c253437074
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-23 15:25:31 +0700

    upload flutter_grid

---
+++ 14_Flutter Layout/praktikum/flutter_grid/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override 
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget{
+  const Homepage({Key? key}) : super(key: key);
+  
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      
+    );
+  }
+}
