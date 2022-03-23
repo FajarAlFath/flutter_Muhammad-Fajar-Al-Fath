9cb18bd54341a2b8e3df288e42b8f77c62108fe3
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-23 15:13:56 +0700

    revisi flutter_list

---
+++ 14_Flutter Layout/praktikum/flutter_list/lib/model.dart
@@ -0,0 +1,24 @@
+class Model {
+  final String nama;
+  final String no;
+
+  Model({
+    required this.nama,
+    required this.no,
+  });
+}
+
+final List<Model> items = [
+  Model(nama: 'Annisa', no: '+62 5367 0942'),
+  Model(nama: 'sayed', no: '+62 5367 0645'),
+  Model(nama: 'Ali', no: '+62 5367 0345'),
+  Model(nama: 'Raudah', no: '+62 3453 0942'),
+  Model(nama: 'Ridho', no: '+62 2342 0942'),
+  Model(nama: 'Andika', no: '+62 7876 0942'),
+  Model(nama: 'Raudah', no: '+62 3453 0942'),
+  Model(nama: 'Ridho', no: '+62 2342 0942'),
+  Model(nama: 'Andika', no: '+62 7876 0942'),
+  Model(nama: 'Raudah', no: '+62 3453 0942'),
+  Model(nama: 'Ridho', no: '+62 2342 0942'),
+  Model(nama: 'Andika', no: '+62 7876 0942'),
+];
