677d74706eac33ea47848d67fe482b3a7f9d594c
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-03-25 16:23:57 +0700

    revisi section assets

--- 18_Flutter Assets/praktikum/flutter_assets/lib/main.dart
+++ 18_Flutter Assets/praktikum/flutter_assets/lib/main.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'image.dart';
+import 'image_2.dart';
+import 'image_3.dart';
+import 'image_4.dart';
 
-void main()=> runApp(const MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  @override 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      home: const Homepage(),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => const Homepage(),
+        '/image': (context) => const Imagepage(),
+      },
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('assets Demo'),
+        backgroundColor: Colors.green,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          SizedBox(
+            width: 20,
+            height: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/imagepage');
+              },
+              child: const Image(
+                image: AssetImage('assets/1.jpg'),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
