80a94f97a84047f5512de1df9dd456662bc7af3d
FajarAlfath <fjralfath11@gmail.com>
2022-03-30 14:14:00 +0700

    revisi code form button

--- 16_Form Input Button/praktikum/flutter_form/lib/list_contact.dart
+++ 16_Form Input Button/praktikum/flutter_form/lib/list_contact.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form/add_contact.dart';
+import 'list_data.dart';
 
 class Listcontact extends StatelessWidget {
   const Listcontact({Key? key}) : super(key: key);
@@ -8,12 +10,48 @@ class Listcontact extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('List Widget'),
+        backgroundColor: Colors.lightGreen,
         centerTitle: true,
         leading: InkWell(
           child: const Icon(Icons.chevron_left),
           onTap: () => {Navigator.pop(context)},
         ),
       ),
+      body: ListView.separated(
+        itemBuilder: (context, i){
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 20,
+              child: Text(data[i].nama[0],
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 12,
+                fontWeight: FontWeight.w300
+              ),),
+              backgroundColor: Colors.lightGreen,
+            ),
+            title: Text(data[i].nama,
+            style: const TextStyle(fontWeight: FontWeight.bold),),
+            subtitle: Text(data[i].noHp),
+          );
+        },
+        separatorBuilder: (context, i){
+          return const Divider();
+        },
+        itemCount: data.length,
+        ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.lightGreen,
+        onPressed: (){
+          Navigator.push(context,
+          MaterialPageRoute(builder: (BuildContext context) => const AddContact()),);
+        },
+      ),
     );
   }
 }
+  
---
+++ 16_Form Input Button/praktikum/flutter_form/lib/list_data.dart
@@ -0,0 +1,13 @@
+class ListData {
+  String nama;
+  String noHp;
+  
+  ListData(this.nama,this.noHp);
+}
+
+List<ListData> data = [
+  ListData('Alfath','+62 2345 6432'),
+  ListData('Annisa','+62 8767 4543'),
+  ListData('Andika','+62 2543 6765'),
+  ListData('Radit','+62 2325 3454'),
+];
