8e3e5e135881ad7c858a6e369f14c25596c7f9c1
FajarAlfath <fjralfath11@gmail.com>
2022-03-30 14:21:54 +0700

    revisi code form button

--- 16_Form Input Button/praktikum/flutter_form/lib/add_contact.dart
+++ 16_Form Input Button/praktikum/flutter_form/lib/add_contact.dart
@@ -10,6 +10,15 @@ class AddContact extends StatefulWidget{
 class _AddContactState extends State<AddContact>{
   @override 
   Widget build(BuildContext context){
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 100, left: 35),
+        child: Column(
+          children: [
+
+          ],
+        ),
+        ),
+    );
   }
 }
