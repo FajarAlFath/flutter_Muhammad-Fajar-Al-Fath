0928c202012cad5a3c2a8553ab31f6cfb16a74fd
FajarAlfath <fjralfath11@gmail.com>
2022-04-04 19:26:28 +0700

    revisi code nav

---
+++ 19_Flutter Navigation/praktikum/flutter_nav/lib/add.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_nav/homepage.dart';
+
+class Add extends StatefulWidget {
+  const Add({Key? key}) : super(key: key);
+
+  @override
+  State<Add> createState() => _AddState();
+}
+
+class _AddState extends State<Add> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller1 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            inputForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget inputForm() {
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: const EdgeInsets.all(60),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller,
+              keyboardType: TextInputType.name,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "Masukkan nama anda",
+                labelText: 'Nama Anda',
+                icon: const Icon(Icons.people),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nama tidak boleh kosong';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: controller1,
+              keyboardType: TextInputType.phone,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "0821xxxxxxxxx",
+                labelText: 'Nomor Telepon',
+                icon: const Icon(Icons.phone),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nomor tidak boleh kosong';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height: 30,
+              child: ElevatedButton.icon(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Data Disimpan')));
+                    }
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Homepage(
+                                  namaUser: controller.text,
+                                  nomorUser: controller1.text,
+                                )));
+                  },
+                  icon: const Icon(Icons.save),
+                  label: const Text('Save your data')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- 19_Flutter Navigation/praktikum/flutter_nav/lib/homepage.dart
+++ 19_Flutter Navigation/praktikum/flutter_nav/lib/homepage.dart
@@ -1,20 +1,55 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_nav/add.dart';
 
-class Homepage extends StatefulWidget{
-  const Homepage({Key? key}) : super(key: key);
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key, required this.namaUser, required this.nomorUser})
+      : super(key: key);
 
-
-  @override 
-  _HomepageState createState ()=> _HomepageState();
-}
-
-class _HomepageState extends State<Homepage>{
-  @override 
-  Widget build(BuildContext context){
+  final String namaUser;
+  final String nomorUser;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: ListView(
+        children: <Widget>[
+          dataContact('Muhammad Fajar Al Fath', '082356704923'),
+          dataContact('Annisa', '082356704923'),
+          dataContact('Nabila', '082356704923'),
+          dataContact('Robot', '082356704923'),
+          dataContact(namaUser, nomorUser),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (BuildContext context) => const Add()),
+          );
+        },
+      ),
+    );
+  }
+
+  //widget data contact to add your contact hardcode
+  Widget dataContact(var name, String number) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: const CircleAvatar(
+            backgroundImage: AssetImage(''),
+          ),
+          subtitle: Text(
+            number,
+            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 20),
+          ),
+          title: Text(name),
+        ),
+        const Divider(),
+      ],
     );
   }
 }
