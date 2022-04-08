856269cf92a20ba46d68a5536340e00a3bfaca41
Muhammad Fajar Al Fath <fjralfath11@gmail.com>
2022-04-09 01:59:31 +0700

    revisi code animation

---
+++ 22_Flutter Animation/praktikum/flutter_animation2/lib/screens/homepage.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'addpage.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key, required this.namaUser, required this.nomorUser})
+      : super(key: key);
+
+  final String namaUser;
+  final String nomorUser;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          dataContact('Muhammad Fajar Al Fath', '082356704923'),
+          dataContact('Annisa', '082356704923'),
+          dataContact('Nabila', '082356704923'),
+          dataContact('Robot', '082356704923'),
+          dataContact(namaUser, nomorUser),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (BuildContext context) => const Add()),
+          );
+        },
+      ),
+    );
+  }
+
+  //widget data contact to add your contact hardcode
+  Widget dataContact(var name, String number) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: const CircleAvatar(
+            backgroundImage: AssetImage('assets/img/1.png'),
+          ),
+          subtitle: Text(
+            number,
+            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 20),
+          ),
+          title: Text(name),
+        ),
+        const Divider(),
+      ],
+    );
+  }
+}
